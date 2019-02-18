06236c4f9e8c3eb6cd4bfc31d8eeba891a5a74fc
jianqiao.ms <jianqiao.ms@foxmail.com>
2019-02-18 17:04:48 +0800

    dev

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 module.exports = {
     hooks: {
         config: function(config) {
-            config.styles = config.styles || config.pluginsConfig['theme-light'].styles;
+
 
             return config;
         }
