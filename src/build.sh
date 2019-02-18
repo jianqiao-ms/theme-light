e885451bcc0ca072bbb74dfad5c5c583d36a1476
jianqiao.ms <jianqiao.ms@foxmail.com>
2019-02-18 16:49:07 +0800

    dev

--- src/build.sh
+++ src/build.sh
@@ -1,30 +1,30 @@
 #! /bin/bash
 
 # Cleanup folder
-rm -rf _assets
+rm -rf assets
 
 # Recreate folder
-mkdir -p _assets/website/
-mkdir -p _assets/ebook/
+mkdir -p assets/website/
+mkdir -p assets/ebook/
 
 # Compile JS
-browserify src/js/core/index.js | uglifyjs -mc > _assets/website/gitbook.js
-browserify src/js/theme/index.js | uglifyjs -mc > _assets/website/theme.js
+browserify src/js/core/index.js | uglifyjs -mc > assets/website/gitbook.js
+browserify src/js/theme/index.js | uglifyjs -mc > assets/website/theme.js
 
 # Compile Website CSS
-lessc -clean-css src/less/website.less _assets/website/style.css
+lessc -clean-css src/less/website.less assets/website/style.css
 
 # Compile eBook CSS
-lessc -clean-css src/less/ebook.less _assets/ebook/ebook.css
-lessc -clean-css src/less/pdf.less _assets/ebook/pdf.css
-lessc -clean-css src/less/mobi.less _assets/ebook/mobi.css
-lessc -clean-css src/less/epub.less _assets/ebook/epub.css
+lessc -clean-css src/less/ebook.less assets/ebook/ebook.css
+lessc -clean-css src/less/pdf.less assets/ebook/pdf.css
+lessc -clean-css src/less/mobi.less assets/ebook/mobi.css
+lessc -clean-css src/less/epub.less assets/ebook/epub.css
 
 # Copy fonts
-mkdir -p _assets/website/fonts
-cp -R node_modules/font-awesome/fonts/ _assets/website/fonts/fontawesome/
+mkdir -p assets/website/fonts
+cp -R node_modules/font-awesome/fonts/ assets/website/fonts/fontawesome/
 
 # Copy icons
-mkdir -p _assets/website/images
-cp node_modules/gitbook-logos/output/favicon.ico _assets/website/images/
-cp node_modules/gitbook-logos/output/apple-touch-icon-152.png _assets/website/images/apple-touch-icon-precomposed-152.png
+mkdir -p assets/website/images
+cp node_modules/gitbook-logos/output/favicon.ico assets/website/images/
+cp node_modules/gitbook-logos/output/apple-touch-icon-152.png assets/website/images/apple-touch-icon-precomposed-152.png
